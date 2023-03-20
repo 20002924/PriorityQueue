27fd40255d8354f5c72603b80098f12d8e7020ce
20002924 <>
2023-03-20 18:36:11 +0000

    Third commit

---
+++ src/queuemanager/UnsortedLinkedPriorityQueue.java
@@ -0,0 +1,107 @@
+package queuemanager;
+
+/**
+ * Implementation of the PriorityQueue ADT using a sorted array for storage.
+ *
+ * Because Java does not allow generic arrays (!), this is implemented as an
+ * array of Object rather than of PriorityItem&lt;T&gt;, which would be natural.
+ * Array elements accessed then have to be cast to PriorityItem&lt;T&gt; before
+ * using their getItem() or getPriority() methods.
+ * 
+ * This is an example of Java's poor implementation getting in the way. Java
+ * fanboys will no doubt explain at length why it has to be this way, but note
+ * that Eiffel allows it because Eiffel generics were done right from the start,
+ * rather than being tacked on as an afterthought and limited by issues of
+ * backward compatibility. Humph!
+ * 
+ * @param <T> The type of things being stored.
+ */
+public class UnsortedLinkedPriorityQueue<T> implements PriorityQueue<T> {
+    
+    /**
+     * Where the data is actually stored.
+     */
+    private final Object[] storage;
+
+    /**
+     * The size of the storage array.
+     */
+    private final int capacity;
+
+    /**
+     * The index of the last item stored.
+     *
+     * This is equal to the item count minus one.
+     */
+    private int tailIndex;
+
+    /**
+     * Create a new empty queue of the given size.
+     *
+     * @param size
+     */
+    public UnsortedLinkedPriorityQueue(int size) {
+        storage = new Object[size];
+        capacity = size;
+        tailIndex = -1;
+    }
+
+    @Override
+    public T head() throws QueueUnderflowException {
+        if (isEmpty()) {
+            throw new QueueUnderflowException();
+        } else {
+            int i = tailIndex;
+            while (((PriorityItem<T>) storage[i]).getPriority() < ((PriorityItem<T>) storage[i-1]).getPriority()) {
+                i = i - 1;
+            }
+            return ((PriorityItem<T>) storage[i]).getItem();
+        }
+    }
+
+    @Override
+    public void add(T item, int priority) throws QueueOverflowException {
+        tailIndex = tailIndex + 1;
+        if (tailIndex >= capacity) {
+            /* No resizing implemented, but that would be a good enhancement. */
+            tailIndex = tailIndex - 1;
+            throw new QueueOverflowException();
+        } else {
+            int i = tailIndex;
+            storage[i] = new PriorityItem<>(item, priority);
+        }
+    }
+
+    @Override
+    public void remove() throws QueueUnderflowException {
+        if (isEmpty()) {
+            throw new QueueUnderflowException();
+        } else {
+            //Search for and delete the highest priority item in the queue
+            int i = tailIndex;
+            while (((PriorityItem<T>) storage[i]).getPriority() < ((PriorityItem<T>) storage[i-1]).getPriority()) {
+                i = i - 1;
+                storage[i] = storage[i + 1];
+            }
+            tailIndex = tailIndex - 1;
+        }
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return tailIndex < 0;
+    }
+
+    @Override
+    public String toString() {
+        String result = "[";
+        for (int i = 0; i <= tailIndex; i++) {
+            if (i > 0) {
+                result = result + ", ";
+            }
+            result = result + storage[i];
+        }
+        result = result + "]";
+        return result;
+    }
+}
